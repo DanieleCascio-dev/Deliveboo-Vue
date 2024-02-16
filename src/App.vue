0e6a78871ec410ba3ac7d492f20f4a3846626fcd
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-02-16 16:21:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script></script>
+
+<template>
+  <div class="container">
+    <h2 class="mt-5">DELIVEBOO</h2>
+    <i class="fa-solid fa-magnifying-glass"></i>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+@import "@fortawesome/fontawesome-free/css/all.css";
+</style>
