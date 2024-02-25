973834b558ccf57cec593edc13ff5c45cc79c2ae
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-02-25 16:51:54 +0100

    add: hero on HomePage, cooment categories

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,40 +1,41 @@
 <script>
-import axios from 'axios';
-import { store } from '../store';
-import CategoryCard from '../components/CategoryCard.vue';
-import AppMobile from '../components/AppMobile.vue';
+import axios from "axios";
+import { store } from "../store";
+import CategoryCard from "../components/CategoryCard.vue";
+import AppMobile from "../components/AppMobile.vue";
+import HomeHero from "../components/HomeHero.vue";
 export default {
   data() {
     return {
       store,
-      categories: []
-    }
+      categories: [],
+    };
   },
   created() {
     //chiamata axios
-    axios.get(`${this.store.baseUrl}/api/categories`)
-      .then((resp) => {
-        //console.log(resp);
-        this.categories = resp.data.results;
-      })
+    axios.get(`${this.store.baseUrl}/api/categories`).then((resp) => {
+      //console.log(resp);
+      this.categories = resp.data.results;
+    });
   },
-  components: { CategoryCard, AppMobile }
+  components: { CategoryCard, AppMobile, HomeHero },
 };
-
 </script>
 
 <template>
-  <div class="container">
+  <HomeHero />
+  <!--  <div class="container">
     <h3 class="text-center">CATEGORIES:</h3>
     <div class="row row-cols-4">
-      <div class="col" v-for="category in categories" :key="category.id">
-        <!-- component category card -->
-        <CategoryCard :category="category" />
+      <div class="col" v-for="category in categories" :key="category.id"> -->
+
+  <!-- component category card -->
+
+  <!--  <CategoryCard :category="category" />
       </div>
-
     </div>
+  </div> -->
 
-  </div>
   <AppMobile />
 </template>
 
