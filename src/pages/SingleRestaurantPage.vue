3105e5eb65be4fda036784c7b0c33918a034f199
grossiValentina <grossivalentina435@gmail.com>
2024-02-28 20:15:32 +0100

    add:layout wip2

--- src/pages/SingleRestaurantPage.vue
+++ src/pages/SingleRestaurantPage.vue
@@ -196,97 +196,87 @@ export default {
 
                     <div class="card-restaurant-details card me-3 py-3 px-5 rounded border-0 text-white">
                         <div class="content-info">
-                        <h2>{{ curRestaurant.name }}</h2>
-                        <p>{{ curRestaurant.address }}</p>
-                        <hr>
-                        <p>Notes: The restaurant does not accept meal vouchers.</p>
+                            <h2>{{ curRestaurant.name }}</h2>
+                            <p>{{ curRestaurant.address }}</p>
+                            <hr>
+                            <p>Notes: The restaurant does not accept meal vouchers.</p>
                         </div>
                     </div>
 
                     <!-- ******************************* CART ************************ -->
                     <div class="card-cart card py-3 px-5 rounded border-0 text-white text-center">
-                        <h2>Your order</h2>
+                        <div>
+                            <h2>Your order</h2>
+                        </div>
                         <hr>
-                        <ul>
-                            <li v-for="product in storageMeal">
-                                <h4>
-                                    {{ product.name }}
-                                    <span><button class="remove-btn btn rounded-circle" @click="removeMeal(product)">
-                                            X
-                                        </button></span>
-                                </h4>
+                        <div class="scrollbar">
+                            <ul>
+                                <li v-for="product in storageMeal">
+                                    <h4>
+                                        {{ product.name }}
+                                        <span><button class="remove-btn btn rounded-circle" @click="removeMeal(product)">
+                                                X
+                                            </button></span>
+                                    </h4>
 
-                                        <p>Price: {{ product.price.toFixed(2) }}€</p>
-                                        <p>Quantity: {{ product.quantity }}</p>
-                                        <p>
-                                            <strong>Restaurant: </strong>
-                                            {{ product.restaurant }}
-                                        </p>
-                                    </li>
-                                </ul>
-                                <h4 class="mb-4">Tot: {{ totPrice.toFixed(2) }}€</h4>
-                                <div class="cart-btn d-flex justify-content-center">
-                                    <button class="clear-btn btn me-5 w-75" @click="clear()">Clear</button>
-                                    <button class="checkout-btn btn w-75">Go to payment</button>
-                                </div>
-                            </div>
+                                    <p>Price: {{ product.price.toFixed(2) }}€</p>
+                                    <p>Quantity: {{ product.quantity }}</p>
+                                    <p>
+                                        <strong>Restaurant: </strong>
+                                        {{ product.restaurant }}
+                                    </p>
+                                </li>
+                            </ul>
                         </div>
-                        <!-- ****************************** END CART ********************* -->
-
-                        <!--MENU-->
-                        <div class="row">
-                            <div class="col-12">
-
-                                <div class="pb-3">
-                                    <h2 class="pb-3">Menu</h2>
-
-                                    <div class="row">
-                                        <div class="col-lg-6 col-md-12 col-sm-12">
-
-                                            <div class="menu card card-menu">
-                                                <h2 class="py-3 text-white text-center">Menu</h2>
-                                                <!--CARD PER I PIATTI-->
-                                                <ul v-for="meal in curRestaurant.meals" :key="meal.id"
-                                                    class="list-unstyled card card-single-meal p-3 border-0 text-white mb-3">
-                                                    <li class="fw-bold mb-2 fs-5">{{ meal.name }}</li>
-                                                    <li><img :src="meal.image" style="height: 150px; max-width: 100%;" />
-                                                    </li>
-                                                    <li>
-                                                        <p class="fst-italic pt-4">Ingredients: {{ meal.description }}</p>
-                                                    </li>
-                                                    <li>
-                                                        <p class="fw-bold">{{ meal.price }}€</p>
-                                                    </li>
-                                                    <li>
-                                                        <p class="">{{ meal.is_active ? "Available" : "Not Available" }}</p>
-                                                    </li>
-                                                    <li>
-                                                        <button class="buy-btn btn" :disabled="!meal.is_active"
-                                                            @click="checkRestaurant(meal)">
-                                                            Buy
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
+                            <h4 class="mb-4">Tot: {{ totPrice.toFixed(2) }}€</h4>
+                            <div class="cart-btn d-flex justify-content-center">
+                                <button class="clear-btn btn me-5 w-75" @click="clear()">Clear</button>
+                                <button class="checkout-btn btn w-75">Go to payment</button>
                             </div>
-                        </div>
                     </div>
+                    <!-- ****************************** END CART ********************* -->
+                </div>
+                <!--MENU-->
 
-                    <!-- MODAL -->
-                    <div class="_fixed _modal" :class="{ 'd-none': showModal == false }">
-                        <h3>
-                            Wait! You are in another restaurant! Before to add new meals to your order
-                            you have to clear your cart.
-                        </h3>
-                        <button class="btn btn-warning" @click="clearAndAdd">Clear cart</button>
-                        <button class="btn btn-success" @click="hideModal">Don't buy</button>
-                    </div>
+                <div class="menu card card-menu">
+                    <h2 class="py-3 text-white text-center">Menu</h2>
+                    <!--CARD PER I PIATTI-->
+                    
+                    <ul v-for="meal in curRestaurant.meals" :key="meal.id"
+                        class="list-unstyled card card-single-meal p-3 border-0 text-white mb-3">
+                        <li class="fw-bold mb-2 fs-5">{{ meal.name }}</li>
+                        <li><img :src="meal.image" style="height: 150px; max-width: 100%;" />
+                        </li>
+                        <li>
+                            <p class="fst-italic">Ingredients: {{ meal.description }}</p>
+                        </li>
+                        <li>
+                            <p class="fw-bold">{{ meal.price }}€</p>
+                        </li>
+                        <li>
+                            <p>{{ meal.is_active ? "Available" : "Not Available" }}</p>
+                        </li>
+                        <li>
+                            <button class="buy-btn btn" :disabled="!meal.is_active" @click="checkRestaurant(meal)">
+                                Buy
+                            </button>
+                        </li>
+                    </ul>
+            
                 </div>
             </div>
+        </div>
+    </div>
+
+    <!-- MODAL -->
+    <div class="_fixed _modal" :class="{ 'd-none': showModal == false }">
+        <h3>
+            Wait! You are in another restaurant! Before to add new meals to your order
+            you have to clear your cart.
+        </h3>
+        <button class="btn btn-warning" @click="clearAndAdd">Clear cart</button>
+        <button class="btn btn-success" @click="hideModal">Don't buy</button>
+    </div>
 </template>
 
 
@@ -313,35 +303,53 @@ export default {
 
 
     .card-menu {
-        width: 60%;
+        width: 100%;
         background-color: $card-violet;
         padding: 0 3rem;
 
-        
-
         .card-single-meal {
             background-color: $primary-green;
-            
+            font-size: 10px;
+            p {
+                padding-top: 5px;
+                margin: 0;
+            }
+
+            @include response("md") {
+                font-size: 20px;
+                p {
+                    padding-top: 10px;
+                    margin: 0;
+                }
+            }
 
             img {
                 width: 200px;
+
+                @include response("md") {
+                    width: 300px;
+                }
             }
         }
 
         .buy-btn {
             background-color: $secondary-violet;
             color: white;
+            margin-top: 5px;
         }
 
+        @include response("md") {
+            width: 60%;
+        }
     }
 
     .flex {
         position: relative;
         top: -50px;
-        @include response("md"){
+
+        @include response("md") {
             display: flex;
         }
-
 
         .card-restaurant-details {
             background-color: $card-violet;
@@ -351,20 +359,35 @@ export default {
                 border: 1px solid white;
             }
 
-           @include response("md"){
-            width: 60%;
-           }
+            @include response("md") {
+                width: 60%;
+            }
         }
 
         .card-cart {
-            background-color: $card-violet;
+            background-color: $primary-violet;
             width: 100%;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            z-index: 100;
+            .scrollbar{
+                overflow-y: auto;
+                max-height: 20vh;
+            }
 
-            @include response("md"){
-            width: 30%;
-            margin-top: 30px;
-            
-           }
+            @include response("md") {
+                width: 30%;
+                position: relative;
+                position: fixed;
+                background-color: $card-violet;
+                height: 50vh;
+                
+                .scrollbar{
+                    overflow-y: auto;
+                    max-height: 20vh;
+                }
+            }
 
             hr {
                 border: 1px solid white;
@@ -388,9 +411,6 @@ export default {
                 background-color: $primary-green;
                 color: white;
             }
-
-            // Rendi il carrello fisso
-
         }
     }
 
