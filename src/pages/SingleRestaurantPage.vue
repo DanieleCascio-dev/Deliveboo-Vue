f6641b0f803231887fe3830e95e832dd7098db50
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-02-29 22:55:31 +0100

    add: simple layout

--- src/pages/SingleRestaurantPage.vue
+++ src/pages/SingleRestaurantPage.vue
@@ -60,6 +60,7 @@ export default {
         localStorage.setItem(
           meal.name,
           JSON.stringify({
+            id: meal.id,
             name: meal.name,
             image: meal.image,
             description: meal.description,
@@ -76,6 +77,7 @@ export default {
         localStorage.setItem(
           meal.name,
           JSON.stringify({
+            id: meal.id,
             name: meal.name,
             image: meal.image,
             description: meal.description,
@@ -137,6 +139,7 @@ export default {
           localStorage.setItem(
             meal.name,
             JSON.stringify({
+              id: meal.id,
               name: meal.name,
               image: meal.image,
               description: meal.description,
@@ -172,312 +175,330 @@ export default {
   },
 };
 </script>
+
 <template>
-  <!--WRAPPER HERO-->
-  <div>
+  <!-- WRAPPER -->
+  <div class="wrapper">
     <div v-if="loading">
       <h3 class="text-center">Loading...</h3>
     </div>
-    <div v-else>
-      <div class="image-hero d-flex flex-column">
-        <img :src="curRestaurant.image" />
+    <!-- HERO IMG -->
+    <div class="hero-img">
+      <img v-if="curRestaurant" :src="curRestaurant.image" alt="" />
+    </div>
+
+    <div class="row justify-content-between px-5 w-100">
+      <!-- TITLE CARD -->
+      <div v-if="curRestaurant" class="_title-card col-6 text-center p-0">
+        <h2>{{ curRestaurant.name }}</h2>
+        <p>{{ curRestaurant.address }}</p>
+        <p>
+          <strong>Telephone: </strong>
+          {{ curRestaurant.phone }}
+        </p>
       </div>
-      <!--CONTAINER PER I PIATTI-->
-      <div class="my-container">
-        <!--CARD INFO RISTORANTE-->
-        <!-- ******************** CARD INFO RESTAURANT ********************-->
-        <div
-          class="flex justify-content-center text-center align-items-start align-self-stretch"
-        >
-          <div
-            class="card-restaurant-details card me-3 py-3 px-5 rounded border-0 text-white"
-          >
-            <div class="content-info">
-              <h2>{{ curRestaurant.name }}</h2>
-              <p>{{ curRestaurant.address }}</p>
-              <hr />
-              <p>Notes: The restaurant does not accept meal vouchers.</p>
-            </div>
-          </div>
-          <!-- ******************** / CARD INFO RESTAURANT ********************-->
-          <!-- ******************************* CART ************************ -->
-          <div
-            class="card-cart card py-3 px-5 mt-3 rounded border-0 text-white text-center"
-          >
-            <p class="your-order">Your order</p>
-            <!--BUTTON ^ FOR ORDER-->
-            <div class="scrollbar btn-group dropup">
-              <button
-                type="button"
-                class="btn dropdown-toggle btn-order"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Order
-              </button>
-              <!--LIST ORDER MEALS-->
-              <ul class="dropdown-menu">
-                <li v-for="product in storageMeal">
-                  <h4>
-                    {{ product.name }}
-                    <span
-                      ><button
-                        class="remove-btn btn rounded-circle"
-                        @click="removeMeal(product)"
-                      >
-                        X
-                      </button></span
-                    >
-                  </h4>
-                  <p>Price: {{ product.price.toFixed(2) }}€</p>
-                  <p>Quantity: {{ product.quantity }}</p>
-                  <p>
-                    <strong>Restaurant: </strong>
-                    {{ product.restaurant }}
-                  </p>
-                </li>
-              </ul>
-            </div>
-            <!--TOT AND BUTTON "CLEAR" E "PAYMENT"-->
-            <p class="mb-4 Tot">Tot: {{ totPrice.toFixed(2) }}€</p>
-            <div class="cart-btn d-flex justify-content-between">
-              <button class="clear-btn btn btn-sm" @click="clear()">
-                Clear
-              </button>
-              <button class="checkout-btn btn btn-sm">Go to payment</button>
-            </div>
-          </div>
-          <!-- ****************************** END CART ********************* -->
-          <!-- ****************************** / CART ********************* -->
-        </div>
-        <!--MENU-->
-        <!-- ****************************** MENU ****************************** -->
-        <div class="menu card card-menu">
-          <h2 class="py-3 text-white text-center">Menu</h2>
-          <!--CARD PER I PIATTI-->
-          <!--CARD MEAL-->
-          <ul
-            v-for="meal in curRestaurant.meals"
-            :key="meal.id"
-            class="list-unstyled card card-single-meal p-3 border-0 text-white mb-3"
-          >
-            <li class="fw-bold mb-2 fs-5">{{ meal.name }}</li>
-            <li>
-              <img :src="meal.image" style="height: 150px; max-width: 100%" />
-            </li>
-            <li>
-              <p class="fst-italic">Ingredients: {{ meal.description }}</p>
-            </li>
-            <li>
-              <p class="fw-bold">{{ meal.price }}€</p>
-            </li>
-            <li>
-              <p>{{ meal.is_active ? "Available" : "Not Available" }}</p>
-            </li>
-            <li>
-              <button
-                class="buy-btn btn"
-                :disabled="!meal.is_active"
-                @click="checkRestaurant(meal)"
-              >
-                Buy
-              </button>
+      <!-- END TITLE CARD -->
+      <!-- CART -->
+      <div class="col-4 d-flex flex-column cart py-2">
+        <div class="p-0 text-center">
+          <h2>Your Order</h2>
+          <hr />
+          <ul v-if="storageMeal.length > 0">
+            <li class="meal text-start" v-for="product in storageMeal">
+              <h5>{{ product.name }}</h5>
+              <p>Price: {{ product.price }}</p>
+              <p>
+                Quantity: {{ product.quantity }}
+
+                <!-- buttons -->
+
+                <span class="btn btn-danger" @click="removeMeal(product)">
+                  <i class="fa-solid fa-minus"> </i>
+                </span>
+                <span class="btn btn-success" @click="checkRestaurant(product)">
+                  <i class="fa-solid fa-plus"></i>
+                </span>
+              </p>
             </li>
           </ul>
         </div>
-        <!-- ****************************** / MENU ****************************** -->
+        <div class="payment">
+          <h4>Total: {{ totPrice }} €</h4>
+          <button class="btn btn-warning">Payment</button>
+        </div>
       </div>
     </div>
+    <!-- END CART -->
   </div>
-  <!-- MODAL -->
-  <!-- ****************************** MODAL ****************************** -->
-  <div class="_fixed _modal" :class="{ 'd-none': showModal == false }">
-    <h3>
-      Wait! You are in another restaurant! Before to add new meals to your order
-      you have to clear your cart.
-    </h3>
-    <button class="btn btn-warning" @click="clearAndAdd">Clear cart</button>
-    <button class="btn btn-success" @click="hideModal">Don't buy</button>
+  <div class="container mb-5">
+    <div class="menu text-start p-5">
+      <!-- Menu card -->
+      <div
+        v-for="meal in curRestaurant.meals"
+        :key="meal.id"
+        class="meal-card row d-flex mb-4"
+      >
+        <div
+          class="img-meal col-5 d-flex justify-content-center align-items-center p-0"
+        >
+          <img :src="meal.image" alt="" />
+        </div>
+        <div class="card-content col-7 p-3">
+          <h4>{{ meal.name }}</h4>
+          <p>Ingredients: {{ meal.description }}</p>
+          <p>Price: {{ meal.price }} €</p>
+          <button @click="checkRestaurant(meal)" class="btn btn-success">
+            Add to cart
+          </button>
+        </div>
+      </div>
+      <!-- end Menu card -->
+    </div>
   </div>
-  <!-- ****************************** MODAL ****************************** -->
+  <!-- END WRAPPER -->
 </template>
+
 <style lang="scss" scoped>
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixin" as *;
-.image-hero {
-  .img {
+
+.wrapper {
+  width: 100%;
+}
+.hero-img {
+  width: 100%;
+  height: auto;
+  img {
     width: 100%;
     height: auto;
-    object-fit: cover;
   }
 }
-.my-container {
-  width: 100%;
-  height: 100%;
-  padding-left: 40px;
-  padding-right: 40px;
-  background-color: $secondary-green;
-  padding-bottom: 3rem;
-  .card-menu {
-    width: 100%;
-    background-color: $card-violet;
-    padding: 0 3rem;
-    // card-menu-responsive-tablet
-    @include response("md") {
-      width: 60%;
-      position: relative;
-      left: 130px;
-    }
-    // card-menu-responsive-desktop
-    @include response("l") {
-      position: relative;
-      left: 0;
-    }
-    .card-single-meal {
-      background-color: $primary-green;
-      font-size: 10px;
-      p {
-        padding-top: 5px;
-        margin: 0;
-      }
-      // card-single-meal-responsive-tablet
-      @include response("md") {
-        font-size: 20px;
-        p {
-          padding-top: 10px;
-          margin: 0;
-        }
-      }
-      // image-card-single-meal-responsive-tablet
-      img {
-        width: 200px;
-        @include response("md") {
-          width: 300px;
-        }
-      }
-    }
-    .buy-btn {
-      background-color: $secondary-violet;
-      color: white;
-      margin-top: 5px;
-    }
-  }
-  .flex {
-    position: relative;
-    top: -50px;
-    // flex-responsive-tablet
-    @include response("md") {
-      display: flex;
-    }
-    .card-restaurant-details {
-      background-color: $card-violet;
-      width: 100%;
-      // card-restaurant-details-responsive-tablet
-      @include response("md") {
-        width: 60%;
-      }
-      // card-restaurant-details-responsive-desktop
-      @include response("l") {
-        position: relative;
-        right: 180px;
-      }
-      hr {
-        border: 1px solid white;
-      }
-    }
-    .card-cart {
-      background-color: $primary-violet;
-      width: 100%;
-      height: 40px;
-      position: fixed;
-      bottom: 300px; //* METTI A 0 *//
-      left: 0;
-      z-index: 100;
-      font-size: 10px;
-      .your-order {
-        display: none;
-      }
-      .Tot {
-        position: relative;
-        bottom: 6px;
-        font-size: 12px;
-      }
-      .scrollbar {
-        //display: none;
-        .btn-order {
-          //width: 20px;
-          position: relative;
-          right: 140px;
-          bottom: 12px;
-          color: white;
-          font-size: 12px;
-        }
-      }
-      // card-cart-responsive-tablet
-      @include response("md") {
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        z-index: 100;
-        font-size: 20px;
-        .scrollbar {
-          overflow-y: auto;
-          max-height: 20vh;
-        }
-      }
+._title-card {
+  width: 500px;
+  height: 250px;
+  background-color: $card-violet;
+  color: white;
+  border-radius: 15px;
+  position: relative;
+  bottom: 40px;
+}
+.cart {
+  width: 300px;
+  height: 250px;
+  background-color: $card-violet;
+  color: white;
+  border-radius: 15px;
+  position: relative;
+  bottom: 40px;
+  overflow-y: auto;
 
-      // card-cart-responsive-desktop
-      @include response("l") {
-        width: 30%;
-        position: fixed;
-        top: 16%;
-        left: 65%;
-        background-color: $card-violet;
-        height: 50vh;
-        font-size: 30px;
-      }
-      hr {
-        border: 1px solid white;
-      }
-      ul {
-        list-style-type: none;
-        .remove-btn {
-          background-color: $primary-green;
-          color: white;
-        }
-      }
-      .cart-btn {
-        //* BOTTONI   *//
-        position: relative;
-        bottom: 54px;
-      }
-      .clear-btn {
-        background-color: $primary-green;
-        color: white;
-        display: none;
-      }
-      .checkout-btn {
-        background-color: $primary-green;
-        color: white;
-        font-size: 12px;
-        margin-top: 1.5px;
-        position: relative;
-        left: 220px;
+  .payment {
+  }
+
+  li {
+    padding: 2px 10px;
+    p {
+      font-size: 18px;
+    }
+    p,
+    h2 {
+      margin: 0;
+    }
+  }
+}
+
+ul {
+  list-style: none;
+}
+
+.menu {
+  min-height: 300px;
+  width: 100%;
+  background-color: $secondary-violet;
+  border-radius: 15px;
+  color: white;
+
+  .meal-card {
+    background-color: $primary-green;
+    width: 70%;
+    border-radius: 15px;
+    .img-meal {
+      img {
+        border-radius: 10px;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
       }
     }
   }
-  ._fixed {
-    width: 100%;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  ._modal {
-    background-color: rgba(128, 128, 128, 0.591);
-  }
 }
+// .my-container {
+//   width: 100%;
+//   height: 100%;
+//   padding-left: 40px;
+//   padding-right: 40px;
+//   background-color: $secondary-green;
+//   padding-bottom: 3rem;
+//   .card-menu {
+//     width: 100%;
+//     background-color: $card-violet;
+//     padding: 0 3rem;
+//     // card-menu-responsive-tablet
+//     @include response("md") {
+//       width: 60%;
+//       position: relative;
+//       left: 130px;
+//     }
+//     // card-menu-responsive-desktop
+//     @include response("l") {
+//       position: relative;
+//       left: 0;
+//     }
+//     .card-single-meal {
+//       background-color: $primary-green;
+//       font-size: 10px;
+//       p {
+//         padding-top: 5px;
+//         margin: 0;
+//       }
+//       // card-single-meal-responsive-tablet
+//       @include response("md") {
+//         font-size: 20px;
+//         p {
+//           padding-top: 10px;
+//           margin: 0;
+//         }
+//       }
+//       // image-card-single-meal-responsive-tablet
+//       img {
+//         width: 200px;
+//         @include response("md") {
+//           width: 300px;
+//         }
+//       }
+//     }
+//     .buy-btn {
+//       background-color: $secondary-violet;
+//       color: white;
+//       margin-top: 5px;
+//     }
+//   }
+//   .flex {
+//     position: relative;
+//     top: -50px;
+//     // flex-responsive-tablet
+//     @include response("md") {
+//       display: flex;
+//     }
+//     .card-restaurant-details {
+//       background-color: $card-violet;
+//       width: 100%;
+//       // card-restaurant-details-responsive-tablet
+//       @include response("md") {
+//         width: 60%;
+//       }
+//       // card-restaurant-details-responsive-desktop
+//       @include response("l") {
+//         position: relative;
+//         right: 180px;
+//       }
+//       hr {
+//         border: 1px solid white;
+//       }
+//     }
+//     .card-cart {
+//       background-color: $primary-violet;
+//       width: 100%;
+//       height: 40px;
+//       position: fixed;
+//       bottom: 300px; //* METTI A 0 *//
+//       left: 0;
+//       z-index: 100;
+//       font-size: 10px;
+//       .your-order {
+//         display: none;
+//       }
+//       .Tot {
+//         position: relative;
+//         bottom: 6px;
+//         font-size: 12px;
+//       }
+//       .scrollbar {
+//         //display: none;
+//         .btn-order {
+//           //width: 20px;
+//           position: relative;
+//           right: 140px;
+//           bottom: 12px;
+//           color: white;
+//           font-size: 12px;
+//         }
+//       }
+//       // card-cart-responsive-tablet
+//       @include response("md") {
+//         position: fixed;
+//         bottom: 0;
+//         left: 0;
+//         z-index: 100;
+//         font-size: 20px;
+//         .scrollbar {
+//           overflow-y: auto;
+//           max-height: 20vh;
+//         }
+//       }
+
+//       // card-cart-responsive-desktop
+//       @include response("l") {
+//         width: 30%;
+//         position: fixed;
+//         top: 16%;
+//         left: 65%;
+//         background-color: $card-violet;
+//         height: 50vh;
+//         font-size: 30px;
+//       }
+//       hr {
+//         border: 1px solid white;
+//       }
+//       ul {
+//         list-style-type: none;
+//         .remove-btn {
+//           background-color: $primary-green;
+//           color: white;
+//         }
+//       }
+//       .cart-btn {
+//         //* BOTTONI   *//
+//         position: relative;
+//         bottom: 54px;
+//       }
+//       .clear-btn {
+//         background-color: $primary-green;
+//         color: white;
+//         display: none;
+//       }
+//       .checkout-btn {
+//         background-color: $primary-green;
+//         color: white;
+//         font-size: 12px;
+//         margin-top: 1.5px;
+//         position: relative;
+//         left: 220px;
+//       }
+//     }
+//   }
+//   ._fixed {
+//     width: 100%;
+//     height: 100vh;
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//   }
+//   ._modal {
+//     background-color: rgba(128, 128, 128, 0.591);
+//   }
+// }
 </style>
